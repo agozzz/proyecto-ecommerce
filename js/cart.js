a72e81a540d37695509fd0038bd258dc20c088ee
JSCostabel <152417274+JSCostabel@users.noreply.github.com>
2024-08-23 20:01:07 -0300

    Provisorio1
    Catalogo autos, provisorio.

---
+++ js/cart.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const url_autos = 'https://japceibal.github.io/emercado-api/cats_products/101.json';
+ // Función para obtener los productos de la categoría autos(101)
+    function obtenerProductos(){
+    fetch(url_autos)
+    .then(response => response.json())
+    .then(data => mostrarProductos(data.products))
+    .catch(error => console.error('Error al mostrar los autos: ', error));
+}
+ // Función para mostrar los productos en el contenedor
+function mostrarProductos(productos) {
+    const contenedor = document.getElementById("autos-container");
+    contenedor.innerHTML = ""; // Limpiar el contenido previo
+
+    productos.forEach(producto => {
+        // Estructura de la tarjeta
+        const card = document.createElement("div");
+        card.className = "card";
+        card.innerHTML = `
+            <img src="${producto.image}" alt="${producto.name}">
+            <div class="card-body">
+                <h5 class="card-title">${producto.name}</h5>
+                <p class="card-text">${producto.description}</p>
+                <p class="card-precio">$${producto.cost}</p>
+                <p class="card-sold">${producto.soldCount} vendidos</p>
+            </div>
+        `;
+        contenedor.appendChild(card);
+    });
+}
+
+// Llamada a la función para obtener productos al cargar la página
+obtenerProductos();
+});
