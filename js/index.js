3486b61b52d9c8836251eff72e912b2b81831c7e
Agostina Zorzoli Galeano <agozorzoli@gmail.com>
2024-08-28 10:23:02 -0300

    Ajustes CSS y Desafíate

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
-window.onload = function() {
-    let usuario = sessionStorage.getItem("username");
-    let contraseña = sessionStorage.getItem("password");
+/*DESAFÍATE - ENTREGA 1*/
+window.onload = function() { /*Al cargar la ventana se ejecuta la siguiente función*/
+    let usuario = localStorage.getItem("username"); /*Tomamos del localStorage los datos de usuario y contraseña*/
+    let contraseña = localStorage.getItem("password");
 
-    if (!usuario || !contraseña) {
+    if (!usuario || !contraseña) { /*Acá se ejecuta una condición, si no hay valor ingresado como usuario o contraseña, la página nos redireccionará al login*/
         window.location.href = "login.html";
     }
 };
 
+
 document.addEventListener("DOMContentLoaded", function(){
+    /*DESAFÍATE -ENTREGA 2*/
+    let mostrarUsuario = document.getElementById("mostrar-usuario"); /*Traemos el elemento del index.html donde vamos a mostrar el nombre de usuario*/
+    let usuario = localStorage.getItem("username"); /*Traemos desde el localStorage el valor ingresado como usuario*/
+    
+    mostrarUsuario.innerHTML += "Hola," + " " + usuario + "!";/*Modificamos el html para que en mostrarUsuario se muuestre el saludo junto con el nombre de usuario*/
+
+    
 
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
