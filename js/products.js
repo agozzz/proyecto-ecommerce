daf1e7b504553672609e99d74375038a89b5c794
Sofia Taroco <sofiataroco94@gmail.com>
2024-09-11 21:58:14 -0300

    buscador con filtro

--- js/products.js
+++ js/products.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const url_autos = 'https://japceibal.github.io/emercado-api/cats_products/101.json';
+    let productos = [];
  // Función para obtener los productos de la categoría autos(101)
-    function obtenerProductos(){
+function obtenerProductos(){
     fetch(url_autos)
     .then(response => response.json())
-    .then(data => mostrarProductos(data.products))
+    .then(data => {
+        productos = data.products; // Guardar los productos en la variable global
+        mostrarProductos(productos); // Mostrar todos los productos inicialmente
+    })
     .catch(error => console.error('Error al mostrar los autos: ', error));
 }
  // Función para mostrar los productos en el contenedor
@@ -33,6 +37,26 @@ function mostrarProductos(productos) {
     });
 }
 
-// Llamada a la función para obtener productos al cargar la página
+   // Función para filtrar los productos en función de la búsqueda
+function filtrarProductos(busqueda) {
+    const productosFiltrados = productos.filter(producto => {
+        const titulo = producto.name.toLowerCase();
+        const descripcion = producto.description.toLowerCase();
+        return titulo.includes(busqueda.toLowerCase()) || descripcion.includes(busqueda.toLowerCase());
+    });
+    mostrarProductos(productosFiltrados); // Mostrar solo los productos filtrados
+}
+
+// Llama a la función para obtener productos al cargar la página
 obtenerProductos();
+
+// Busca el campo de búsqueda y se agrega el evento input para filtrar productos
+const campoBusqueda = document.getElementById("search");
+if (campoBusqueda) {
+    campoBusqueda.addEventListener("input", function() {
+        filtrarProductos(campoBusqueda.value);
+    });
+} else {
+    console.error("El campo de búsqueda no fue encontrado.");
+}
 });
