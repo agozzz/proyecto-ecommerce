922edd7d8f29c0a3c56d9cd2a00078073cf96e55
Sofia Taroco <sofiataroco94@gmail.com>
2024-09-27 15:19:39 -0300

    mostrar productos relacionados

--- js/product-info.js
+++ js/product-info.js
@@ -15,6 +15,8 @@ function obtenerProducto(productID) {
         let producto = data;
         console.log(`Producto seleccionado:`, producto); // Mostrar los productos obtenidos en la consola
         mostrarProducto(producto); // Mostrar los detalles del producto
+        mostrarProductosRelacionados(producto.relatedProducts); // Mostrar productos relacionados
+
     })
     .catch(error => {
         console.error("Error al obtener el producto", error);
@@ -42,4 +44,36 @@ function mostrarProducto(producto) {
 function seleccionarImagen(imagen) {
     let imagenPrincipal = document.getElementById('imagen-principal');
     imagenPrincipal.src = imagen.src;
+}
+
+// Mostrar los productos relacionados
+function mostrarProductosRelacionados(relatedProducts) {
+    console.log("Productos relacionados:", relatedProducts); // Para verificar qué productos llegan
+    let productosRelacionadosContainer = document.getElementById('productos-relacionados');
+    productosRelacionadosContainer.innerHTML = '';
+
+    // Mostrar todos los productos relacionados que se devuelven desde la API
+    if (relatedProducts && relatedProducts.length > 0) {
+        relatedProducts.forEach(product => {
+            let productoHTML = `
+                <div class="col">
+                    <div class="card h-100 text-center custom-card cursor-active" onclick="actualizarProducto(${product.id})">
+                        <img class="card-img-top img-thumbnail" src="${product.image}" alt="Imagen de ${product.name}" style="height: 150px; object-fit: cover;">
+                        <div class="card-body">
+                            <h5 class="card-title">${product.name}</h5>
+                        </div>
+                    </div>
+                </div>
+            `;
+            productosRelacionadosContainer.innerHTML += productoHTML;
+        });
+    } else {
+        console.log("No hay productos relacionados.");
+    }
+}
+
+// Actualizar la página con el nuevo producto seleccionado
+function actualizarProducto(productID) {
+    localStorage.setItem("prodID", productID); // Guardar el nuevo ID de producto en localStorage
+    window.location.reload(); // Recargar la página para mostrar el nuevo producto
 }
